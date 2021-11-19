f4d5c25576074415dd4ab56d918a8c2295fb56dc
Brandon De Leon <brandondeleon.615@gmail.com>
2021-11-19 15:27:01 -0600

    finished exercise 9

---
+++ total_sales_{year}.sql
@@ -0,0 +1,9 @@
+-- What are the respective total sales for each of those years?
+-- HINT: SUM
+-- ex 9
+
+
+
+select sum(total) as total from Invoice where invoice.InvoiceDate BETWEEN datetime('2009-01-01 00:00:00') and datetime('2009-12-31 00:00:00');
+select sum(total) as total from Invoice where invoice.InvoiceDate BETWEEN datetime('2010-01-01 00:00:00') and datetime('2010-12-31 00:00:00');
+select sum(total) as total from Invoice where invoice.InvoiceDate BETWEEN datetime('2011-01-01 00:00:00') and datetime('2011-12-31 00:00:00');
